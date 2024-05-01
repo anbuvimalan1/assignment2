eaaeb7c629a9f94d33b3a4dd5a0338b8917efa1e
anbuvimalan1 <167068347+anbuvimalan1@users.noreply.github.com>
2024-05-01 13:04:56 +0400

    Add files via upload

---
+++ organization/party/Election.java
@@ -0,0 +1,43 @@
+package assignment2.organizations.party;
+
+/**
+ * TODO finish this class. (0.25 points)
+ */
+public class Election {
+    private int votersNumber;
+
+    private boolean fair;
+
+    private String description;
+
+
+    public Election(int votersNumber, boolean fair, String description) {
+        this.votersNumber = votersNumber;
+        this.fair = fair;
+        this.description = description;
+    }
+
+    public int getVotersNumber() {
+        return votersNumber;
+    }
+
+    public void setVotersNumber(int votersNumber) {
+        this.votersNumber = votersNumber;
+    }
+
+    public boolean isFair() {
+        return fair;
+    }
+
+    public void setFair(boolean fair) {
+        this.fair = fair;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
